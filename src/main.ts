626cd0be06e4d09d3dad1028a8afbf6dbadef37d
Gustavo Pauli <68567054+Gustavo-Pauli@users.noreply.github.com>
2025-06-04 16:50:30 -0300

    change default ip

--- src/main.ts
+++ src/main.ts
@@ -5,26 +5,37 @@ import { AppModule } from './app.module'
 import { NestExpressApplication } from '@nestjs/platform-express'
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    bodyParser: false,
-  })
+  try {
+    console.log('Starting application with env:', {
+      PORT: process.env.PORT,
+      TRUSTED_ORIGINS: process.env.TRUSTED_ORIGINS,
+      MONGO_URI: process.env.MONGO_URI ? '*****' : undefined,
+    })
+    const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+      bodyParser: false,
+    })
 
-  app.set('query parser', 'extended')
-  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }))
+    app.set('query parser', 'extended')
+    app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }))
 
-  const configService = app.get(ConfigService)
-  const port = configService.get<number>('PORT') ?? 3000
+    const configService = app.get(ConfigService)
+    const port = configService.get<number>('PORT') ?? 3000
 
-  const trustedOrigins = (process.env.TRUSTED_ORIGINS as string).split(',')
+    const trustedOrigins = process.env.TRUSTED_ORIGINS
+      ? (process.env.TRUSTED_ORIGINS as string).split(',')
+      : ['*']
 
-  app.enableCors({
-    origin: trustedOrigins,
-    credentials: true,
-  })
+    app.enableCors({
+      origin: trustedOrigins,
+      credentials: true,
+    })
 
-  // app.setGlobalPrefix('api', { exclude: ['/api/auth/{*path}'] })
-
-  await app.listen(port)
+    await app.listen(port, '0.0.0.0')
+    console.log(`Application is running on http://0.0.0.0:${port}`)
+  } catch (err) {
+    console.error('Error during app bootstrap:', err)
+    process.exit(1)
+  }
 }
 
 void bootstrap()
